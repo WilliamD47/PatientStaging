43d9334c3a7d18f6fbe7d874ecf237a669a99a45
WilliamD47 <69767121+WilliamD47@users.noreply.github.com>
2021-12-14 20:35:16 +0000

    almost finished stripe

--- js/main.js
+++ js/main.js
@@ -1,86 +1,140 @@
-var PatientData = [
-    {
-        "firstName": "James",
-        "surName": "Blunt",
-        "hospitalNumber": "1234567",
-        "referralDate": "1/04/2021",
-        "testsDates": {
-            "chestXray": "04/05/2021 15:01",
-            "CTThorax": "29/05/2021 16:02",
-            "PET_CT": "02/05/2021 10:55",
-            "EBUS": "28/06/2021 09:55"
+var PatientData = {
+    "firstName": "James",
+    "lastName": "Blunt",
+    "dateOfBirth": "1952-05-11",
+    "hospitalNumber": "1234567",
+    "referralDate": "2021-05-11",
+    "milestones": [
+        {
+            "milestoneType": {
+                "referenceName": "Plain chest X-ray (procedure)",
+                "name": "Chest X-Ray"
+            },
+            "addedAt": "09-05-2021",
+            "updatedAt": "10-05-2021",
+            "completedOn": "10-05-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "Triage",
+                "name": "Triage"
+            },
+            "addedAt": "15-05-2021",
+            "updatedAt": "15-05-2021",
+            "completedOn": "15-05-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "CT of Thorax",
+                "name": " CT thorax "
+            },
+            "addedAt": "21-05-2021",
+            "updatedAt": "23-05-2021",
+            "completedOn": "23-05-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "Clinic",
+                "name": "clinic"
+            },
+            "addedAt": "24-05-2021",
+            "updatedAt": "25-05-2021",
+            "completedOn": "25-05-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "PET CT of whole body",
+                "name": "PET-CT"
+            },
+            "addedAt": "25-05-2021",
+            "updatedAt": "06-06-2021",
+            "completedOn": "06-06-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "Endobronchial ultrasonography guided transbronchial needle aspiration",
+                "name": "EBUS"
+            },
+            "addedAt": "07-05-2021",
+            "updatedAt": "07-06-2021",
+            "completedOn": "09-06-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "Specialist multidisciplinary team",
+                "name": "MDT"
+            },
+            "addedAt": "09-06-2021",
+            "updatedAt": "11-06-2021",
+            "completedOn": "11-06-2021",
+            "currentState": "COMPLETED"
+        },
+        {
+            "milestoneType": {
+                "referenceName": "Thoracic surgery",
+                "name": "Surgery"
+            },
+            "addedAt": "11-06-2021",
+            "updatedAt": "31-06-2021",
+            "completedOn": "30-06-2021",
+            "currentState": "COMPLETED"
         }
-    },
-    {
-        "firstName": "Dave",
-        "surName": "Blunt",
-        "hospitalNumber": "1234567",
-        "referralDate": "11/05/2021",
-        "testsDates": {
-            "chestXray": "10/05/2021 15:01",
-            "CTThorax": "21/05/2021 16:02",
-            "PET_CT": "30/05/2021 10:55",
-            "EBUS": "05/06/2021 09:55"
-        }
-    },
-    {
-        "firstName": "Bob",
-        "surName": "Blunt",
-        "hospitalNumber": "1234567",
-        "referralDate": "11/07/2021",
-        "testsDates": {
-            "chestXray": "10/03/2021 15:01",
-            "CTThorax": "21/03/2021 16:02",
-            "PET_CT": "30/03/2021 10:55",
-            "EBUS": "05/04/2021 09:55"
-        }
-    },
-    {
-        "firstName": "Jenny",
-        "surName": "Blunt",
-        "hospitalNumber": "1234567",
-        "referralDate": "11/03/2021",
-        "testsDates": {
-            "chestXray": "4/05/2021 15:01",
-            "CTThorax": "8/05/2021 16:02",
-            "PET_CT": "23/05/2021 10:55",
-            "EBUS": "12/06/2021 09:55"
-        }
-    },
-    {
-        "firstName": "Eric",
-        "surName": "Smith",
-        "hospitalNumber": "1234567",
-        "referralDate": "11/04/2021",
-        "testsDates": {
-            "chestXray": "9/06/2021 15:01",
-            "CTThorax": "20/06/2021 16:02",
-            "PET_CT": "29/06/2021 10:55",
-            "EBUS": "4/07/2021 09:55"
-        }
-    }
-];
+    ]
+};
 function AddToHTML(personData) {
-    var patient = {
+    personData["milestones"].forEach(element => {
+        var part1 = String(element["addedAt"]).slice(0, 2);
+        var part2 = String(element["addedAt"]).slice(3, 5);
+        var part3 = String(element["addedAt"]).slice(6, 10);
+        element["addedAt"] = part3 + "-" + part2 + "-" + part1;
+        part1 = String(element["updatedAt"]).slice(0, 2);
+        part2 = String(element["updatedAt"]).slice(3, 5);
+        part3 = String(element["updatedAt"]).slice(6, 10);
+        element["updatedAt"] = part3 + "-" + part2 + "-" + part1;
+        part1 = String(element["completedOn"]).slice(0, 2);
+        part2 = String(element["completedOn"]).slice(3, 5);
+        part3 = String(element["completedOn"]).slice(6, 10);
+        element["completedOn"] = part3 + "-" + part2 + "-" + part1;
+    });
+    const patient = {
         firstName: personData["firstName"],
-        lastName: personData["surName"],
-        referralDate: new Date(personData["referralDate"])
+        lastName: personData["lastName"],
+        birthDate: new Date(personData["dateOfBirth"]),
+        referralDate: new Date(personData["referralDate"]),
+        hospitalNumber: personData["hospitalNumber"],
+        milestones: personData["milestones"]
     };
-    var fullName = patient.firstName + " " + patient.lastName;
-    var tableBody = document.getElementById("tbody");
+    const fullName = patient.firstName + " " + patient.lastName;
+    const tableBody = document.getElementById("tbody");
     var row = tableBody.insertRow(0);
     // Making Rows
-    var nameCell = row.insertCell(0);
+    const nameCell = row.insertCell(0);
     nameCell.innerHTML = fullName;
-    var middleCell = row.insertCell(1);
-    var dateCell = row.insertCell(2);
-    dateCell.innerHTML = patient.referralDate.toLocaleDateString();
+    const middleCell = row.insertCell(1);
+    const dateCell = row.insertCell(2);
+    dateCell.innerHTML = patient.birthDate.toLocaleDateString();
     var fullTable = '';
-    // patient.dates.forEach(element => {
-    // });
-    console.log(fullName);
+    var lengths = [];
+    patient.milestones.forEach(element => {
+        // Get difference between dates
+        var first = new Date(element.addedAt);
+        var last = new Date(element.completedOn);
+        var secondsDifference = last.getTime() - first.getTime();
+        if (secondsDifference != 0) {
+            secondsDifference = (secondsDifference / 200) * 2;
+        }
+        lengths.push(secondsDifference / 86400);
+    });
+    console.log(lengths);
 }
-window.onload = function () {
+window.onload = () => {
     'use strict';
-    PatientData.forEach(function (element) { return AddToHTML(element); });
+    AddToHTML(PatientData);
 };
+//# sourceMappingURL=main.js.map
