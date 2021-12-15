f788cffbcc6c3aae6569cf6e5b218febfdeed591
WilliamD47 <69767121+WilliamD47@users.noreply.github.com>
2021-12-15 19:31:21 +0000

    Update main.ts

--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,3 @@
-
 var PatientData = {
     "firstName": "James",
     "lastName": "Blunt",
@@ -87,128 +86,93 @@ var PatientData = {
             "currentState": "COMPLETED"
         }
     ]
-}
-interface Milestone {
-    milestoneType: {
-        referenceName: String,
-        name: String
-    }
-    addedAt: Date,
-    updatedAt: Date,
-    completedOn: Date,
-    currentState: String
-}
-
-interface StripeSegment {
-    length: Number,
-    colourClass: String,
-    startDate: String,
-    endDate: String,
-    type: String
-}
-
-
-interface Patient {
-    firstName: String,
-    lastName: String,
-    birthDate: Date,
-    referralDate: Date,
-    hospitalNumber: Number,
-    milestones: [Milestone]
-}
-
+};
 function AddToHTML(personData) {
     personData["milestones"].forEach(element => {
-        var part1 = String(element["addedAt"]).slice(0, 2)
-        var part2 = String(element["addedAt"]).slice(3, 5)
-        var part3 = String(element["addedAt"]).slice(6, 10)
-        element["addedAt"] = part3 + "-" + part2 + "-" + part1
-        part1 = String(element["updatedAt"]).slice(0, 2)
-        part2 = String(element["updatedAt"]).slice(3, 5)
-        part3 = String(element["updatedAt"]).slice(6, 10)
-        element["updatedAt"] = part3 + "-" + part2 + "-" + part1
-        part1 = String(element["completedOn"]).slice(0, 2)
-        part2 = String(element["completedOn"]).slice(3, 5)
-        part3 = String(element["completedOn"]).slice(6, 10)
-        element["completedOn"] = part3 + "-" + part2 + "-" + part1
+        var part1 = String(element["addedAt"]).slice(0, 2);
+        var part2 = String(element["addedAt"]).slice(3, 5);
+        var part3 = String(element["addedAt"]).slice(6, 10);
+        element["addedAt"] = part3 + "-" + part2 + "-" + part1;
+        part1 = String(element["updatedAt"]).slice(0, 2);
+        part2 = String(element["updatedAt"]).slice(3, 5);
+        part3 = String(element["updatedAt"]).slice(6, 10);
+        element["updatedAt"] = part3 + "-" + part2 + "-" + part1;
+        part1 = String(element["completedOn"]).slice(0, 2);
+        part2 = String(element["completedOn"]).slice(3, 5);
+        part3 = String(element["completedOn"]).slice(6, 10);
+        element["completedOn"] = part3 + "-" + part2 + "-" + part1;
     });
-    const patient: Patient = {
+    const patient = {
         firstName: personData["firstName"],
         lastName: personData["lastName"],
         birthDate: new Date(personData["dateOfBirth"]),
         referralDate: new Date(personData["referralDate"]),
         hospitalNumber: personData["hospitalNumber"],
         milestones: personData["milestones"]
-
-    }
-    const fullName = patient.firstName + " " + patient.lastName
-    const tableBody: HTMLTableElement = <HTMLTableElement>document.getElementById("tbody")
-
-    var row = tableBody.insertRow(0)
+    };
+    const fullName = patient.firstName + " " + patient.lastName;
+    const tableBody = document.getElementById("tbody");
+    var row = tableBody.insertRow(0);
     // Making Rows
-    const nameCell = row.insertCell(0)
-    nameCell.innerHTML = fullName
-    var middleCell = row.insertCell(1)
-
-    const dateCell = row.insertCell(2)
-    dateCell.innerHTML = patient.birthDate.toLocaleDateString()
-    var fullTable = ''
-    var lengths = []
-    var endDates = []
-    var elementProgress = 0
+    const nameCell = row.insertCell(0);
+    nameCell.innerHTML = fullName;
+    var middleCell = row.insertCell(1);
+    const dateCell = row.insertCell(2);
+    dateCell.innerHTML = patient.birthDate.toLocaleDateString();
+    var fullTable = '';
+    var lengths = [];
+    var endDates = [];
+    var elementProgress = 0;
     patient.milestones.forEach(element => {
         // Get difference between dates
-        var first = new Date(element.addedAt)
-        var last = new Date(element.completedOn)
+        var first = new Date(element.addedAt);
+        var last = new Date(element.completedOn);
         if (elementProgress != 0) {
-            console.log(first, new Date(patient.milestones[elementProgress - 1].completedOn))
+            console.log(first, new Date(patient.milestones[elementProgress - 1].completedOn));
         }
         if (elementProgress != 0) {
             if (first != new Date(patient.milestones[elementProgress - 1].completedOn)) {
-                const previousLast = new Date(patient.milestones[elementProgress - 1].completedOn)
-                const redStripeSegment: StripeSegment = {
+                const previousLast = new Date(patient.milestones[elementProgress - 1].completedOn);
+                const redStripeSegment = {
                     length: ((first.getTime() - previousLast.getTime()) / 10000000) * 2,
                     colourClass: "red",
                     startDate: previousLast.toLocaleDateString(),
                     endDate: first.toLocaleDateString(),
                     type: ""
-                }
+                };
                 if (redStripeSegment.length > 0) {
-                    lengths.push(redStripeSegment)
+                    lengths.push(redStripeSegment);
                 }
             }
-            
         }
-
-        var secondsDifference = last.getTime() - first.getTime()
-
+        var secondsDifference = last.getTime() - first.getTime();
         if (secondsDifference != 0) {
-            var length = (secondsDifference / 10000000) * 2
-        } else {
-            var length = 0
+            var length = (secondsDifference / 10000000) * 2;
         }
-        const stripeSegment: StripeSegment = {
+        else {
+            var length = 0;
+        }
+        const stripeSegment = {
             length: length,
             colourClass: "grn",
             startDate: first.toLocaleDateString(),
             endDate: last.toLocaleDateString(),
             type: ""
-        }
-        lengths.push(stripeSegment)
-        elementProgress += 1
-    })
-    var totalLength = 0
-    console.log(lengths)
+        };
+        lengths.push(stripeSegment);
+        elementProgress += 1;
+    });
+    var totalLength = 0;
+    console.log(lengths);
     lengths.forEach(element => {
         totalLength += element.length;
         fullTable += '<td class=' + element.colourClass + ' style="width: ' + String(element.length) + 'px"><div class="tooltip">' + element.type + '<span class="tooltiptext">Start:' + String(element.startDate) + '\nEnd:' + element.endDate + '</span></div></td>';
     });
     middleCell.innerHTML = '<table border="0" summary=""><tr id="tr">' + fullTable + '</tr></table>';
-    middleCell.style.width = String((totalLength / 100000 ) + 200) + "px"
+    middleCell.style.width = String((totalLength / 100000) + 200) + "px";
 }
-
 window.onload = () => {
     'use strict';
-
     AddToHTML(PatientData);
-}
+};
+//# sourceMappingURL=main.js.map
